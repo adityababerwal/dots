2eb787f0a5b718630dc715f2c1f76407f5feed4f
adityababerwal <adityababerwal@gmail.com>
2023-06-18 18:08:56 +0530

    auto

---
+++ .config/nvim_custom/configs/lspconfig.lua
@@ -0,0 +1,24 @@
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
+local lspconfig = require("lspconfig")
+local servers = { "pyright", "bashls", "clangd", "cssls" }
+
+for _, lsp in ipairs(servers) do
+	lspconfig[lsp].setup({
+		on_attach = on_attach,
+		capabilities = capabilities,
+	})
+end
+
+-- Without the loop, you would have to manually set up each LSP
+--
+-- lspconfig.html.setup {
+--   on_attach = on_attach,
+--   capabilities = capabilities,
+-- }
+--
+lspconfig.rust_analyzer.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+})
---
+++ .config/nvim_custom/plugins.lua
@@ -0,0 +1,77 @@
+local plugins = {
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			ensure_installed = {
+				"bashls",
+				"shellcheck",
+
+				"clangd",
+				"clang-format",
+
+				"rust-analyzer",
+
+				"pyright",
+				"mypy",
+				"ruff",
+				"black",
+
+				"stylua",
+
+				"cssls",
+
+				"prettierd",
+			},
+		},
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				-- defaults
+				"vim",
+				"lua",
+
+				-- web dev
+				"html",
+				"css",
+				"javascript",
+				"typescript",
+				"tsx",
+				"jsonc",
+				-- "vue", "svelte",
+
+				-- low level
+				"c",
+				"zig",
+				"cpp",
+				"python",
+				"bash",
+			},
+		},
+	},
+
+	-- In order to modify the `lspconfig` configuration:
+	{
+		"neovim/nvim-lspconfig",
+
+		dependencies = {
+			"jose-elias-alvarez/null-ls.nvim",
+			config = function()
+				require("custom.configs.null-ls")
+			end,
+		},
+
+		config = function()
+			require("plugins.configs.lspconfig")
+			require("custom.configs.lspconfig")
+		end,
+	},
+
+	{
+		"vimwiki/vimwiki",
+		lazy = false,
+	},
+}
+
+return plugins
