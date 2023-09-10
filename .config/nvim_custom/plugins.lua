34506bc0a491145e5f26acc0f4b12a19c77bc5be
adityababerwal <adityababerwal@gmail.com>
2023-09-11 02:33:48 +0530

    auto

--- .config/nvim_custom/plugins.lua
+++ .config/nvim_custom/plugins.lua
@@ -1,40 +1,4 @@
 local plugins = {
-	{
-		"rcarriga/nvim-dap-ui",
-		event = "VeryLazy",
-		dependencies = "mfussenegger/nvim-dap",
-		config = function()
-			local dap = require("dap")
-			local dapui = require("dapui")
-			dapui.setup()
-			dap.listeners.after.event_initialized["dapui_config"] = function()
-				dapui.open({})
-			end
-			dap.listeners.before.event_terminated["dapui_config"] = function()
-				dapui.close({})
-			end
-			dap.listeners.before.event_exited["dapui_config"] = function()
-				dapui.close({})
-			end
-		end,
-	},
-	{
-		"jay-babu/mason-nvim-dap.nvim",
-		event = "VeryLazy",
-		dependencies = {
-			"williamboman/mason.nvim",
-			"mfussenegger/nvim-dap",
-		},
-		opts = {
-			handlers = {},
-		},
-	},
-	{
-		"mfussenegger/nvim-dap",
-		config = function(_, _)
-			require("core.utils").load_mappings("dap")
-		end,
-	},
 	-- Mason
 	{
 		"williamboman/mason.nvim",
@@ -45,7 +9,6 @@ local plugins = {
 
 				"clangd",
 				"clang-format",
-				"codelldb",
 
 				"rust-analyzer",
 
@@ -54,6 +17,7 @@ local plugins = {
 				"mypy",
 				"ruff",
 
+				"lua_ls",
 				"stylua",
 
 				"cssls",
